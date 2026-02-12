9f7aff8de9865c4726f38e1899c6965297064f8c
Gokudoma <stephan_wacker@hotmail.com>
2026-02-12 13:59:57 +0100

    added requirement

--- core/settings.py
+++ core/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Security
 SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = os.getenv('DEBUG') == 'True'
+DEBUG = os.getenv('DEBUG') == 'False'
 ALLOWED_HOSTS = ['*']
 
 # Application definition
